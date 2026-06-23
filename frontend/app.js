7cc5cb7773f874bda2eacd35552f8fda2b758029
Divanshu Aggarwal <divanshuaggarwal023@gmail.com>
2026-06-23 10:29:45 +0530

    some additions

--- frontend/app.js
+++ frontend/app.js
@@ -13,6 +13,8 @@ analyzeBtn.addEventListener("click", async () => {
         return;
     }
 
+    try {
+
     const response = await fetch(
         "http://localhost:5000/analyze",
         {
@@ -28,6 +30,40 @@ analyzeBtn.addEventListener("click", async () => {
     );
 
     const data = await response.json();
+
+    document.getElementById(
+        "resultsSection"
+    ).style.display = "block";
+
+    document.getElementById("score").innerText =
+        `${data.matchScore}%`;
+
+    const skillsList =
+        document.getElementById("missingSkills");
+
+    skillsList.innerHTML = "";
+
+    data.missingSkills.forEach(skill => {
+
+        const li =
+            document.createElement("li");
+
+        li.innerText = skill;
+
+        skillsList.appendChild(li);
+
+        });
+
+    }
+    catch(error){
+
+        alert("Unable to connect to backend.");
+
+        console.error(error);
+
+    }
+
+    document.getElementById("resultsSection").style.display = "block";
 
     document.getElementById("score").innerText =
         `${data.matchScore}%`;
